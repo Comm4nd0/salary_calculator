a64f312d0b6262d91f390f52b306d19577dc217b
Marco Baldanaza <marcobaldanza@hotmail.co.uk>
2018-07-30 19:29:48 +0100

    changed some stuff

--- calculator/views.py
+++ calculator/views.py
@@ -26,6 +26,8 @@ def net_calc(request):
             salary = salary * 52
         elif per == 'daily':
             salary = salary * 261
+        elif per == 'hourly':
+            salary = (salary * 8) * 261
 
         if salary > 11850:
             taxable_salary = salary - 11850
@@ -41,14 +43,23 @@ def net_calc(request):
         else:
             net_salary = salary
 
+        # National Insurance
+        if salary > 8424:
+            nic_taxable_salary = salary - 8424
+            twelve_percent_of_salary = (12 * nic_taxable_salary) / 100.0
+            twenty_percent_bracket = taxable_salary - twenty_percent_of_salary
+            net_salary = twenty_percent_bracket + 11850
+
         data = {'salary': round(salary, 2),
                 'monthly_salary': round(salary / 12, 2),
                 'weekly_salary': round(salary / 52, 2),
                 'daily_salary': round(salary / 261, 2),
+                'hourly_salary': round((salary / 261) / 8, 2),
                 'net_salary': round(net_salary, 2),
                 'net_monthly_salary': round(net_salary / 12, 2),
                 'net_weekly_salary': round(net_salary / 52, 2),
                 'net_daily_salary': round(net_salary / 261, 2),
+                'net_hourly_salary': round((net_salary / 261) / 8, 2),
                 }
         return HttpResponse(json.dumps(data), content_type='application/json')
     else:
--- salary_calc/urls.py
+++ salary_calc/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from calculator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin', admin.site.urls, name='admin'),
     path('', views.calc, name='calc'),
     path('net', views.net_calc, name='net'),
 ]
